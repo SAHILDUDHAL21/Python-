434fe392af04a1942f0ca62a65c88ac5274302fa
Sahil Dudhal <129080375+SAHILDUDHAL21@users.noreply.github.com>
2024-01-16 21:21:11 +0530

    Add files via upload

---
+++ Graph/graph.py
@@ -0,0 +1,6 @@
+from matplotlib.pyplot import *
+from numpy import *
+x=linspace(0,10)
+y=log10(x)
+plot(x,y)
+show()
---
+++ Graph/graph1.py
@@ -0,0 +1,7 @@
+from matplotlib.pyplot import *
+from numpy import *
+x=arange(0,2*(pi),0.1)
+y=cos(x)
+plot(x,y)
+show()
+
